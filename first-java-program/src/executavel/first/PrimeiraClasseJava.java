38b26f0aa80a4b8a2e70adb633dbc3470defcb2d
Fabiano-Araujo <71131540+fabianofdv@users.noreply.github.com>
2022-06-26 11:09:45 -0300

    Create project one

---
+++ first-java-program/src/executavel/first/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package executavel.first;
+
+public class PrimeiraClasseJava {
+	/*Main é o método auto executável em Java*/
+  public static void main(String[] args) {
+	  
+	  
+	/*Escreve texto no console*/  
+	System.out.println("Meu primeiro programa em Java.");
+    
+} 
+}
